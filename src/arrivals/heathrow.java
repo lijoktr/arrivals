2fc848a51b7dc0a194139eabbe1fd4f3b9c61d6f
lijo <lijomon24@gmail.com>
2022-01-13 14:45:10 +0000

    new

--- src/arrivals/heathrow.java
+++ src/arrivals/heathrow.java
@@ -4,14 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class heathrow {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-System.setProperty("webdriver.chrome.driver", "C:/Users/lijom/Documents/chromedriver.exe");
-		
+		System.setProperty("webdriver.chrome.driver", "C:/Users/lijom/Documents/chromedriver.exe");
+
 		//ChromeDriver driver = new ChromeDriver(); 
 		WebDriver driver = new ChromeDriver(); 
 		driver.get("https://www.qatarairways.com/en-gb/homepage.html");
@@ -24,24 +25,24 @@ System.setProperty("webdriver.chrome.driver", "C:/Users/lijom/Documents/chromedr
 		Thread.sleep(2000);
 		
 		
-		//driver.findElement(By.id("cookie-close-accept-all")).click();
-		driver.findElement(By.id("cookie-close")).click();
-		System.out.println("cookies");
+		driver.findElement(By.id("cookie-close-accept-all")).click();
+		//driver.findElement(By.id("cookie-close")).click();
+		System.out.println("cookies close");
 		
 		Thread.sleep(2000);
 		
 		driver.findElement(By.id("flightstatus-tab")).click();
-		System.out.println("stats");
+		System.out.println("status tab");
 		
 		Thread.sleep(2000);
 				
 		driver.findElement(By.id("flightStatusFrom")).sendKeys("Doha DOH",Keys.TAB, "LHR",Keys.TAB);
-		System.out.println("FLIGHT FROM");
+		System.out.println("departure and arrival details entered");
 		
 		Thread.sleep(2000);
 		
 		driver.findElement(By.xpath("/html/body/div[8]/main/div/div/div/hero-component/div[3]/div/div/div/div/div[4]/div/div[2]/div[4]/div/button")).click();
-		System.out.println("search");
+		System.out.println("search results displayed");
 		
 		Thread.sleep(4000);
 		
